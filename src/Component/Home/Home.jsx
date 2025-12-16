1baa9aa629f839d56818210554341fc9bf2f33e3
Mohamed Behairy <134211075+mohamedbehairy@users.noreply.github.com>
2025-12-16 22:08:09 +0200

    fix import path for vercel build

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import Test from "../test";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setcount] = useState(0);
